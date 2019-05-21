ea869e28b2f9face56719e8a58511e730b831ea1
Perica Prokic <pprokic22@gmail.com>
2019-05-21 08:54:28 +0200

    initializin app

---
+++ routes.py
@@ -0,0 +1,14 @@
+from app import app
+
+
+@app.route('/starwars/api/join')
+def join():
+	return "this is a join route"
+
+@app.route('starwars/api/attack/<int:army_id>')
+def attack(army_id):
+	return "this is the attack route"
+
+@app.route('starwars/api/leave')
+def leave():
+	return "this is a leave route"
