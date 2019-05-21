ee7eea2c928f7bb8f93ddaec594a1133dc47ebe0
Perica Prokic <pprokic22@gmail.com>
2019-05-21 09:08:16 +0200

    ..

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ from app import app
 def join():
 	return "this is a join route"
 
-@app.route('starwars/api/attack/<int:army_id>')
+@app.route('/starwars/api/attack/<int:army_id>')
 def attack(army_id):
 	return "this is the attack route"
 
-@app.route('starwars/api/leave')
+@app.route('/starwars/api/leave')
 def leave():
 	return "this is a leave route"
