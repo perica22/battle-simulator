f649ce8242007cb3a8ccb883f1158186dcf890a8
Perica Prokic <pprokic22@gmail.com>
2019-05-30 10:49:47 +0200

    removed some comment from flask.sh script

--- flask.sh
+++ flask.sh
@@ -23,19 +23,3 @@ sleep 5;
 
 echo 'Running python init script'
 python3 initial_script.py
-
-##########################################################################################
-# In order to stop server running, since it is in a bacground process,
-# that process needs to be stoped explicitly by:
-# searching that process: ps -fA | grep python
-# and than stopping it using: kill <process_id> (process_id from previous command output)
-##########################################################################################
-##########################################################################################
-# pylint issue with DB:
-# Any class you declare as inheriting from db.Modelwon't have
-# query member until the code runs so Pylint can't detect it.
-# Mine soluiton for this is:
-#    - runnning pylint --generate-rcfile>~/.config/pylintrc
-#    - and then find the ignored-modules line,
-#    - rewrite it to: ignored-modules=flask_sqlalchemy
-##########################################################################################
